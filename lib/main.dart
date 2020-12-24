c870ffbe6ff3dd2ab3a68fab051ddc05cd8a62a3
Bernardo Duarte <bernardo.silva@infobase.com.br>
2020-12-23 22:27:19 -0300

    recuperando dados do textfield de forma incorreta

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,9 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
   final _transactions = [
     Transaction(
       id: 't1',
@@ -102,11 +105,13 @@ class MyHomePage extends StatelessWidget {
               child: Column(
                 children: [
                   TextField(
+                    controller: titleController,
                     decoration: InputDecoration(
                       labelText: 'Título',
                     ),
                   ),
                   TextField(
+                    controller: valueController,
                     decoration: InputDecoration(
                       labelText: 'Valor em R\$',
                     ),
@@ -117,7 +122,10 @@ class MyHomePage extends StatelessWidget {
                       FlatButton(
                         textColor: Colors.purple,
                         child: Text('Nova Transalçao'),
-                        onPressed: () {},
+                        onPressed: () {
+                          print(titleController.text);
+                          print(valueController.text);
+                        },
                       ),
                     ],
                   )
